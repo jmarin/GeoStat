3d3fbd4ca8c64469a217cac93740631a00111edf
Juan Marin <juan.marin.otero@gmail.com>
2012-10-24 17:58:13 -0400

    Add request data

--- modules/GeoStatAgents/build.sbt
+++ modules/GeoStatAgents/build.sbt
@@ -5,3 +5,4 @@ organization := "GeoStat"
 //version:= "0.1-SNAPSHOT"
 
 libraryDependencies += "joda-time" % "joda-time" % "1.6.2"
+
--- modules/GeoStatAgents/src/main/scala/org/geostat/agents/Message.scala
+++ modules/GeoStatAgents/src/main/scala/org/geostat/agents/Message.scala
@@ -6,6 +6,23 @@ abstract class Message
 
 case class PingMessage(isAlive: Boolean) extends Message
 
-case class OWSMessage(requestId: Long, serviceType: ServiceType, version: String, operation: String, suboperation: String, resources: String, path: String, queryString: String, httpMethod: String, startTime: DateTime, endTime: DateTime, totalTime: Long, remoteAddress: String, remoteHost: String, host: String, user: String, responseStatus: String, responseContentType: String, failed: Boolean) extends Message {
-}
+sealed trait Status {def name:String}
+
+case object WAITING extends Status {val name = "WAITING"}
+case object RUNNING extends Status {val name = "RUNNING"}
+case object CANCELLING extends Status {val name = "CANCELLING"}
+case object FAILED extends Status {val name = "FAILED"}
+case object FINISHED extends Status {val name = "FINISHED"}
+case object CANCELLED extends Status {val name = "CANCELLED"}
+case object INTERRUPTED extends Status {val name = "INTERRUPTED"}
+
+sealed trait Category {def name:String}
+
+case object OWS extends Category {val name = "OWS"}
+case object REST extends Category {val name = "REST"}
+case object OTHER extends Category {val name = "OTHER"}
+
+case class RequestData(id:Long, status:Status, category:Category, path:String, queryString:String, httpMethod:String, startTime:DateTime, endTime:DateTime, totalTime:Long, remoteAddress:String, remoteHost:String, internalHost:String, remoteUser:String, remoteUserAgent:String, remoteCountry:String, remoteCity:String, remoteLatitude:Double, remoteLongitude:Double, service:String, operation:String, owsOperation:String, subOperation:String, responseContentType:String, errorMessage:String)
+
+
 
